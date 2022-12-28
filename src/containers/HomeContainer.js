e6be02f3355200eb934bfa07d55a39b5e5c9cab2
Rizwan Ullah <rizwan.akhwandzada@gmail.com>
2022-12-28 15:10:16 +0500

    Connecting React with Redux in HomeContainer.js

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,3 +1,25 @@
+/*
+* container binds react with redux
+* in other words, binds actions with components
+* */
+
 import React from "react";
 import Home from "../components/Home";
-export default Home
+import {connect} from 'react-redux'
+import {addToCart} from "../services/Actions/actions";
+
+/*
+* @mapDispatchToProp: When sending data from action to redux store
+* @mapStateToProp: when sending data from redux store to any component
+* @both as const
+* */
+
+const mapStateToProps = state => ({
+    //TODO
+})
+
+const mapDispatchToProps = dispatch=>({
+    addToCartHandler: data => dispatch(addToCart(data))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
--- src/services/Reducers/rootReducer.js
+++ src/services/Reducers/rootReducer.js
@@ -1,3 +1,8 @@
+/*
+* This is a root of all reducer
+* Redux requires only one store so we need to combine all the reducer files
+* reducer combining part is done here.
+* */
 import {combineReducers} from "redux";
 import cartItems from "./reducer";
 export default combineReducers({
