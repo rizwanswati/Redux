1c3f753b23339f4bed0c0be63f31c2ac59edbbcd
Rizwan Ullah <rizwan.akhwandzada@gmail.com>
2022-12-28 13:39:22 +0500

    relativly better designed cart

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import HomeContainer from "./containers/HomeContainer";
 function App() {
   return (
     <div className="App">
+        <h1>Shopping Cart</h1>
       <HomeContainer />
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,18 @@
 import React from "react";
 function Home() {
     return(
-        <div className={"wrapper"}>
-            <div className={"img-wrapper item"}>
+        <div>
+        <div className={"over-wrapper"}>
                 <img src={"https://eagle786llc.com/wp-content/uploads/2021/01/Apple-iPhone-11-1-500x500-1.jpg"}/>
-            </div>
-            <div className={"text-wrapper item"}>
-                <span>I-Phone 10 : $1000</span>
-            </div>
-            <div className={"btn-wrapper item"}>
+
+            <div className={"item"}>
                 <button>Add To Cart</button>
             </div>
+
+            <div className={"item"}>
+                <span>I-Phone 10 : $1000</span>
+            </div>
+        </div>
             <div className={"cart-wrapper"}>
                 <img src={"https://img.freepik.com/premium-vector/vector-shopping-cart-icon-paper-sticker-with-shadow-colored-shopping-symbol-isolated_118339-1774.jpg?w=2000"}/>
             </div>
